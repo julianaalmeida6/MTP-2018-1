3953299adad0938f308bcad472c7b2fe16111fbd
julianaalmeida6 <37781543+julianaalmeida6@users.noreply.github.com>
2018-06-21 20:13:24 -0300

    Update P5.c

--- P5.c
+++ P5.c
@@ -1,86 +1,51 @@
 //Juliana Santana de Almeida
 //Matricula:11721EEl012
+#include <stdio.h>
 
-#include<stdio.h>
-
-void codificar()
-{
-	char str[256]={'\0'};
-	int *ps, i=0;
-	
-	printf("\nDigite uma mensagem para codificar: ");
-	gets(str);
-	char *pmsg=str;
-	printf("Codificada: ");
-	for(i=0;str[i];i++)
-	{
-		ps=(int *)pmsg;
-		if(ps[i]!=0)
-			printf("%d",ps[i]);
-		if(ps[i+1]!=0)
-			printf(", ");
-	}
-}
-
-void decodificar()
-{
+int main()
+{	char str[256];
 	char *p;
-	int q[64], i=0;
-	printf("\nColoque espaco + 0 quando finalizar a entrada de dados!");
-		printf("\nDigite uma mensagem para decodificar : ");
-	
-	for(i=0;i<64;i++)
-	{
-		scanf("%d",&q[i]);
-		getchar();
-		p=(char *)q;
-		if(q[i]==0)
-		break;
+	int i, cont, txt[256], opcao;
+	int *ps;
+	do{
+	do{printf("MENU: \n1 - Criptografar \n2 - Deografar \n3 - Sair do programa \nSua opcao eh: ");
+	scanf("%d", &opcao);
+	getchar();}while(opcao!=1 && opcao!=2 && opcao!=3);
+	i = 0;
+	switch(opcao)
+	{	case 1:
+			for(i=0; i<256; i++){ str[i] = '\0'; }
+			printf("Digite uma informacao textual: ");
+			fgets(str, 256, stdin);
+			for(cont=0; str[cont] != '\0'; cont++);	
+			ps = (int*)str;
+			printf("\nFrase criptografada: \n");
+			for(i=0; i<cont; i++)	
+			{	if(*(ps+i) != 0 )
+				{	printf("%i ", *(ps+i));	}	} 
+			break;
+			
+		case 2:
+			printf("Quando quiser encerrar a sequencia de numeros, entre com 0\n");
+			p = (char*)txt;
+			i=0; cont=0;
+			printf("Digite uma sequencia de numeros: \n");
+			do
+			{	scanf("%d", &cont);
+	 			getchar();
+				txt[i] = cont;
+				i++;
+			}while(cont != 0);
+			p = (char*)&txt;
+			printf("\nFrase deografada: \n");
+			for(i=0; i<sizeof(txt) && *(p+i) != 0; i++)
+			{	printf("%c", *(p+i));		}
+			break;
+
+		default:
+			break;
 	}
-	printf("decodificado: ");
-	printf("%s",p);
-}
- 
-void menu()
-{
-	int op,saida=0;
-	printf("Menu\n\n");
-    printf("1. Codificar\n");
-	printf("2. Decodificar\n");
-	printf("3. Sair.");
-	do
-	{
-
-	    inicio:
-		printf ("\n\nDigite a opcao: ");
-		scanf("%d",&op);
-		getchar();	
-		switch(op)
-		{
-			case 1:
-				codificar();
-				saida=1;
-				break;
-				
-			case 2:
-				decodificar();
-				saida=2;
-				break;
-				
-			case 3:
-				saida=3;
-				break;	
-				
-			default: 
-			printf("Opcao invalida!");
-				goto inicio;				
-				
-		}
-	}while(saida!=3);
-}
-
-int main( )
-{
-	menu();
+	printf("\n\n");
+	}while(opcao != 3);
 	return 0;
 }
