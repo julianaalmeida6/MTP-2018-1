a450b4cb2be6a1803ad7d2815a12434152dac363
julianaalmeida6 <37781543+julianaalmeida6@users.noreply.github.com>
2018-06-21 20:12:21 -0300

    Update P4.c

--- P4.c
+++ P4.c
@@ -1,22 +1,29 @@
 //Juliana Santana de Almeida
 //Matricula:11721EEl012
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main ()
+
 {
-	int cum=0,i;
-	char numero[256];
-	printf("Digite um numero :  ");
-	scanf("%s",&numero); getchar();
-	for(i=0;numero[i]!='\0';i++)
+
+char bits[256];
+int cum=0, i;
+
+printf ("Digite um numero: ");
+scanf ("%s", bits);
+getchar();
+
+for (i=0; bits[i] != '\0'; i++)
+{
+	if(bits[i] >=48 && bits[i]<=57)
 	{
-		if(numero[i]>=48 && numero[i]<=57){
-			numero[i]=numero[i]-'0';
-			cum = cum*10+numero[i];
-		}
-				
+		bits[i]=bits[i]-'0';
+		cum= cum*10 + bits[i];
 	}
-	printf("\n O numero que voce digitou : %d",cum);
-	return 0 ;
+
+}
+
+printf ("O numero digitado: %d", cum);
+return 0;
 }
